07a02ea1e1dcb264fbfaeabf6afb9f905f92bb61
Kseniya Radiseva <Kotaviy@gmail.com>
2023-09-14 20:30:57 +0500

    Test added

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class CardDeliveryTest {
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    @Test
+    void cardDeliveryOrderMustBePlaced() throws InterruptedException {
+        open("http://localhost:9999");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        Thread.sleep(2000);
+        $("[data-test-id='city'] input").setValue("Екатеринбург");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Петрова Евгения");
+        $("[data-test-id='phone'] input").setValue("+79090000000");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $("[data-test-id='notification']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(20))
+                .shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на " + currentDate));
+    }
 }
